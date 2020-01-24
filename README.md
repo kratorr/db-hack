cb5c9cca7a60c5438d31b7fe2188b1318e6589b0
Vladimir <v@ao-sbk.ru>
2020-01-24 10:57:58 +0300

    add db hack script, README

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# db-hack
+# Взламываем электронный дневник
+
+
+Этот репозитрий содержит скрипты для правки электроного дневника из репозитория
+[e-diary](https://github.com/kratorr/e-diary), позволяет исправить оценки, убрать замечания и добавить похвалу.
+
+# Как установить
+
+Установите проект из [e-diary](https://github.com/kratorr/e-diary)
+
+Поместите файл scripts.py в директорию с проектом рядом с файлом manage.py
+
+
+
+# Как использовать
+
+
+Запустите из директории проекта оболочку:
+```bash
+$ python3 manage.py shell
+```
+Далее импортируем функции:
+
+```python
+>>> from scripts import fix_marks, remove_chastisements, create_commendation, 
+```
+Получем учетную запись ученика:
+```python
+>>> schoolkid = get_schoolkid('Фролов Иван')
+```
+
+Меняем оценки:
+```python
+>>> fix_marks(schoolkid)
+```
+Убираем замечания:
+```python
+>>> remove_chastisements(schooldkid)
+```
+Добавляем похвалу:
+```python
+>>> create_commendation(schoolkid, 'Математика')
+```
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DVMN.ORG](https://dvmn.org)
+
---
+++ scripts.py
@@ -0,0 +1,66 @@
+import random
+
+
+from datacenter.models import Schoolkid, Mark, Chastisement
+from datacenter.models import Lesson, Commendation
+from django.core.exceptions import MultipleObjectsReturned
+
+
+text_commendations = [
+    'Молодец!',
+    'Отлично!',
+    'Хорошо!',
+    'Гораздо лучше, чем я ожидал!',
+    'Ты меня приятно удивил!',
+    'Великолепно!',
+    'Прекрасно!',
+    'Ты, как всегда, точен!',
+    'Очень хороший ответ!',
+    'Талантливо!',
+    'Ты сегодня прыгнул выше головы!',
+]
+
+
+def get_schoolkid(full_name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=full_name)
+        return schoolkid
+    except MultipleObjectsReturned:
+        print('Найдено более одного ученика, попробуйте уточнить запрос')
+
+
+def fix_marks(schoolkid):
+    schoolkid_bad_marks = Mark.objects.filter(
+        schoolkid=schoolkid, points__in=[2,3]
+    )
+    for mark in schoolkid_bad_marks:
+        mark.points = 5
+        mark.save()
+
+
+def remove_chastisements(schoolkid):
+    schoolkid_chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    schoolkid_chastisements.delete()
+
+
+def create_commendation(schoolkid, subject_name):
+    subject = Subject.objects.get(
+        title=subject_name,
+        year_of_study=schoolkid.year_of_study
+    )
+
+    lesson = Lesson.objects.filter(
+        subject=subject,
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter
+    ).order_by('-date')[0:1].get()
+
+    new_commendation = Commendation.objects.create(
+        text=random.choice(text_commendations),
+        created=lesson.date,
+        schoolkid=schoolkid,
+        teacher_id=lesson.teacher.id,
+        subject=lesson.subject
+    )
+
+    new_commendation.save()
